4ef04fff50ae4c5774a3c64296d88b07141ac4b3
Mr-Fiddlez <126890289+Intermentality@users.noreply.github.com>
2024-12-11 19:14:00 -0500

    eduts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/einty-iweb',
+  base: '/einty-iweb/',
   plugins: [react()],
 
   assetsInclude: ["**/*.GLSL", "assets/*"],
